ef018ee89f674e796cd13b9669351ec1429af135
Sami Ahmed <90336426+Samiiahmedd@users.noreply.github.com>
2023-10-05 16:46:50 +0300

    Initial Commit

---
+++ NatGeo/NatGeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatGeoApp.swift
+//  NatGeo
+//
+//  Created by Sami Ahmed on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct NatGeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
